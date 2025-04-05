49a8cc1ed87ec6046e2fd2ee1a707d1668cf3162
Barron-Castro-24580137 <mariobarron131@gmail.com>
2025-04-04 20:46:04 -0600

    Add files via upload

---
+++ Practica 1 instalacion/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Practica_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(76, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(260, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Gif animado Practica 1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.rana_imagen_animada_0015;
+            pictureBox1.Location = new Point(76, 56);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(339, 277);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 192);
+            ClientSize = new Size(505, 392);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
