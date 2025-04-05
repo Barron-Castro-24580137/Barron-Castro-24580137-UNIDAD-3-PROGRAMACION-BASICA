8d7ccd809689a02dcc4794dee4512fe6507ea4f1
Barron-Castro-24580137 <mariobarron131@gmail.com>
2025-04-04 20:53:47 -0600

    Add files via upload

---
+++ PRACTICA 10/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace PRACTICA_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(284, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Z= X^2 + X^3";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(192, 255, 255);
+            listBox1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(27, 124);
+            listBox1.Margin = new Padding(3, 2, 3, 2);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(126, 154);
+            listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = Color.FromArgb(192, 255, 255);
+            listBox2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 25;
+            listBox2.Location = new Point(204, 124);
+            listBox2.Margin = new Padding(3, 2, 3, 2);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(125, 154);
+            listBox2.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            listBox3.BackColor = Color.FromArgb(192, 255, 255);
+            listBox3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 25;
+            listBox3.Location = new Point(378, 124);
+            listBox3.Margin = new Padding(3, 2, 3, 2);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(120, 154);
+            listBox3.TabIndex = 3;
+            // 
+            // listBox4
+            // 
+            listBox4.BackColor = Color.FromArgb(192, 255, 255);
+            listBox4.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 25;
+            listBox4.Location = new Point(539, 124);
+            listBox4.Margin = new Padding(3, 2, 3, 2);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(124, 154);
+            listBox4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(79, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 25);
+            label2.TabIndex = 5;
+            label2.Text = "X";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(241, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 25);
+            label3.TabIndex = 6;
+            label3.Text = "X^2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(410, 94);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 25);
+            label4.TabIndex = 7;
+            label4.Text = "X^3";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(588, 94);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 25);
+            label5.TabIndex = 8;
+            label5.Text = "Z";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 128);
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.Location = new Point(284, 298);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 30);
+            button1.TabIndex = 9;
+            button1.Text = "Calcular Valores";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 128);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+    }
+}
